ea7625644324b81a04de492aa9a57a0fe99c390f
Nadi123ya <nadezhda.bdarenko.96@gmail.com>
2022-06-08 13:01:24 +0800

    commited message

--- lesson-21/task4/index.js
+++ lesson-21/task4/index.js
@@ -1,23 +1,19 @@
 export const getTitle = () => {
-  const itemTitle = document.querySelector(".title").textContent;
-  return itemTitle;
+  const itemTitle = document.querySelector(".title");
+  return itemTitle.textContent;
 };
-console.log(getTitle());
 
 export const getDescription = () => {
-  const itemTitle = document.querySelector(".about").innerText;
-  return itemTitle;
+  const itemTitle = document.querySelector(".about");
+  return itemTitle.innerText;
 };
-console.log(getDescription());
 
 export const getPlans = () => {
-  const itemTitle = document.querySelector(".plans").innerHTML;
-  return itemTitle;
+  const itemTitle = document.querySelector(".plans");
+  return itemTitle.innerHTML;
 };
-console.log(getPlans());
 
 export const getGoal = () => {
-  const itemTitle = document.querySelector(".goal").outerHTML;
-  return itemTitle;
+  const itemTitle = document.querySelector(".goal");
+  return itemTitle.outerHTML;
 };
-console.log(getGoal());
