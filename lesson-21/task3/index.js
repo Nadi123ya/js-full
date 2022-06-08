a8eb3373e413fabb4a539c019a65778860c81978
Nadi123ya <nadezhda.bdarenko.96@gmail.com>
2022-06-08 10:34:05 +0800

    commit message

---
+++ lesson-21/task3/index.js
@@ -0,0 +1,13 @@
+export const getItemsList = () => {
+  const elementsList = document.querySelectorAll(".technology");
+  console.dir(elementsList);
+  return elementsList;
+};
+getItemsList();
+
+export const getItemsArray = () => {
+  const elementsArray = document.querySelectorAll(".tool");
+  console.dir(Array.from(elementsArray));
+  return elementsArray;
+};
+getItemsArray();
